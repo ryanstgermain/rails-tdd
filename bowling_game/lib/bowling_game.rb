d1886ff4062e992ef49fc37db23b5e2e5f4a2559
ryanstgermain <ryanjoel26@gmail.com>
2019-05-24 13:19:10 -0600

    wrote code for bowling game. all tests pass so far. ready to refactor

--- bowling_game/lib/bowling_game.rb
+++ bowling_game/lib/bowling_game.rb
@@ -1,2 +1,8 @@
 class BowlingGame
+    def pins(rolls)
+    end
+
+    def score
+        0
+    end
 end
--- bowling_game/spec/bowling_game_spec.rb
+++ bowling_game/spec/bowling_game_spec.rb
@@ -1,4 +1,9 @@
 require_relative '../lib/bowling_game'
 
 describe BowlingGame do
+    it 'scores a gutter game' do
+        game = BowlingGame.new
+        game.pins([0] * 20)
+        expect(game.score).to eq(0)
+    end
 end
