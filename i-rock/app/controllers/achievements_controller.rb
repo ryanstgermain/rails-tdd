e8d026d161f2e725dcadbce78a63242a35d6919f
ryanstgermain <ryanjoel26@gmail.com>
2019-05-29 09:05:31 -0600

    installed redcarpet successfuly, refactored code

--- i-rock/app/controllers/achievements_controller.rb
+++ i-rock/app/controllers/achievements_controller.rb
@@ -14,6 +14,7 @@ class AchievementsController < ApplicationController
 
     def show
         @achievement = Achievement.find(params[:id])
+        @description = Redcarpet::Markdown.new(Redcarpet::Render::HTML).render(@achievement.description)
     end
 
     private
--- i-rock/spec/features/achievement_page_spec.rb
+++ i-rock/spec/features/achievement_page_spec.rb
@@ -3,9 +3,16 @@ require 'spec_helper'
 
 feature 'achievement page' do
     scenario 'achievement public page' do
-        achievement = Achievement.create(title:'Just did it')
+        achievement = FactoryBot.create(:achievement, title: 'Just did it')
         visit("/achievements/#{achievement.id}")
 
         expect(page).to have_content('Just did it')
+    end
+
+    scenario 'render markdown description' do
+        achievement = FactoryBot.create(:achievement, description: 'That *was* hard')
+        visit("/achievements/#{achievement.id}")
+
+        expect(page).to have_content('<em>was</em>')
     end
 end
