73828ccbd9069814e51338e2856cbf8f1a21bb4c
ryanstgermain <ryanjoel26@gmail.com>
2019-05-31 15:28:23 -0600

    initial commit. tests are failing

--- i-rock/spec/models/achievement_spec.rb
+++ i-rock/spec/models/achievement_spec.rb
@@ -9,20 +9,31 @@ RSpec.describe Achievement, type: :model do
             expect(achievement.valid?).to be_falsy
         end
 
-        it "requires title to be unique for one user" do
+        it 'requires title to be unique for one user' do
             user = FactoryBot.build(:user)
             first_achievement = FactoryBot.create(:public_achievement, title: 'First Achievement', user: user)
             new_achievement = Achievement.new(title: 'First Achievement', user: user)
             expect(new_achievement.valid?).to be_falsy
         end
 
-        it "allows different users to have achievements with identical titles" do
+        it 'allows different users to have achievements with identical titles' do
             user1 = FactoryBot.build(:user)
             user2 = FactoryBot.build(:user)
             first_achievement = FactoryBot.create(:public_achievement, title: 'First Achievement', user: user1)
             new_achievement = Achievement.new(title: 'First Achievement', user: user2)
             expect(new_achievement.valid?).to be_truthy
         end
+    end
+
+    it 'belongs to user' do
+        achievement = Achievement.new(title: 'Some title', user: nil)
+        expect(achievement.valid?).to be_falsy
+    end
+
+    it 'has belongs_to user association' do
+        user = FactoryBot.build(:user)
+        achievement = FactoryBot.create(:public_achievement, user: user)
+        expect(achievement.user).to eq(user)
     end
 
 end
