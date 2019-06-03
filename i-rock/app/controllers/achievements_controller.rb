163c3cff323d87abc410ae28e285349f3f484b5a
ryanstgermain <ryanjoel26@gmail.com>
2019-06-03 15:23:01 -0600

    refactored code. tests still arent passing for some reason

--- i-rock/app/controllers/achievements_controller.rb
+++ i-rock/app/controllers/achievements_controller.rb
@@ -1,56 +1,57 @@
 class AchievementsController < ApplicationController
-    before_action :authenticate_user!, only: [ :new, :create, :edit, :update, :destroy ]
-    before_action :owners_only, only: [ :edit, :update, :destroy ]
+  before_action :authenticate_user!, only: [ :new, :create, :edit, :update, :destroy ]
+  before_action :owners_only, only: [ :edit, :update, :destroy ]
 
-    def index
-        @achievements = Achievement.get_public_achievements
+  def index
+    @achievements = Achievement.get_public_achievements
+  end
+
+  def new
+    @achievement = Achievement.new
+  end
+
+  def create
+    service = CreateAchievement.new(params[:achievement], current_user)
+    service.create
+    if service.created?
+      redirect_to achievement_path(service.achievement)
+    else
+      @achievement = service.achievement
+      render :new
     end
+  end
 
-    def new
-        @achievement = Achievement.new
+  def edit
+  end
+
+  def update
+    if @achievement.update_attributes(achievement_params)
+      redirect_to achievement_path(@achievement)
+    else
+      render :edit
     end
+  end
 
-    def create
-        service = CreateAchievement.new(params[:achievement], current_user)
-        service.create
-        if service.created?
-            redirect_to achievement_path(service.achievement)
-        else
-            @achievement = service.achievement
-            render :new
-        end
+  def show
+    @achievement = Achievement.find(params[:id])
+  end
+
+  def destroy
+    @achievement.destroy
+    redirect_to achievements_path
+  end
+
+  private
+
+  def achievement_params
+    params.require(:achievement).permit(:title, :description, :privacy, :cover_image, :featured)
+  end
+
+  def owners_only
+    @achievement = Achievement.find(params[:id])
+    if current_user != @achievement.user
+      redirect_to achievements_path
     end
+  end
 
-    def edit
-    end
-
-    def update
-        if @achievement.update_attributes(achievement_params)
-            redirect_to achievement_path(params[:id])
-        else
-            render :edit
-        end
-    end
-
-    def show
-        @achievement = Achievement.find(params[:id])
-    end
-
-    def destroy
-        @achievement.destroy
-        redirect_to achievements_path
-    end
-
-    private
-
-    def achievement_params
-        params.require(:achievement).permit(:title, :description, :privacy, :cover_image, :featured)
-    end
-
-    def owners_only
-        @achievement = Achievement.find(params[:id])
-        if current_user != @achievement.user
-            redirect_to achievements_path
-        end
-    end
 end
--- i-rock/spec/controllers/achievements_controller_spec.rb
+++ i-rock/spec/controllers/achievements_controller_spec.rb
@@ -3,74 +3,74 @@ require 'spec_helper'
 
 describe AchievementsController do
 
-    describe 'guest user' do
-        describe 'GET index' do
-            let(:achievement) { instance_double(Achievement) }
-      
-            before do
-                allow(Achievement).to receive(:get_public_achievements) { [achievement] }
-            end
+  describe 'guest user' do
+    describe 'GET index' do
+      let(:achievement) { instance_double(Achievement) }
 
-            it 'renders :index template' do
-                get :index
-                expect(response).to render_template(:index)
-            end
+      before do
+        allow(Achievement).to receive(:get_public_achievements) { [achievement] }
+      end
 
-            it 'assigns public achievements to template' do
-                get :index
-                expect(assigns(:achievements)).to eq([achievement])
-            end
-        end
+      it 'renders :index template' do
+        get :index
+        expect(response).to render_template(:index)
+      end
+
+      it 'assigns public achievements to template' do
+        get :index
+        expect(assigns(:achievements)).to eq([achievement])
+      end
     end
-  
-    describe 'authenticated user' do
-        let(:user) { instance_double(User) }
-        before do
-            allow(controller).to receive(:current_user) { user }
-            allow(controller).to receive(:authenticate_user!) { true }
-        end
+  end
 
-        describe 'POST create' do
-            let(:achievement_params) { { title: "title" } }
-            let(:create_achievement) { instance_double(CreateAchievement) }
-            let(:achievement) { instance_double(Achievement) }
-
-            before do
-                allow(CreateAchievement).to receive(:new) { create_achievement }
-                allow(create_achievement).to receive(:create)
-                allow(create_achievement).to receive(:created?)
-                allow(create_achievement).to receive(:achievement) { achievement }
-            end
-
-            it 'sends create message to CreateAchievement' do
-                expect(CreateAchievement).to receive(:new).with(achievement_params, user)
-                expect(create_achievement).to receive(:create)
-                post :create, params: { achievement: achievement_params }
-            end
-
-            context 'achievement is created' do
-                before { allow(create_achievement).to receive(:created?) { true } }
-
-                it 'redirects' do
-                    post :create, params: { achievement: achievement_params }
-                    expect(response.status).to eq(302)
-                end
-            end
-      
-            context 'achievement is not created' do
-                before { allow(create_achievement).to receive(:created?) { false } }
-
-                it 'render :new template' do
-                    post :create, params: { achievement: achievement_params }
-                    expect(response).to render_template(:new)
-                end
-        
-                it 'assigns achievement to template' do
-                    post :create, params: { achievement: achievement_params }
-                    expect(assigns(:achievement)).to eq(achievement)
-                end
-            end
-        end
+  describe 'authenticated user' do
+    let(:user) { instance_double(User) }
+    before do
+      allow(controller).to receive(:current_user) { user }
+      allow(controller).to receive(:authenticate_user!) { true }
     end
+
+    describe "POST create" do
+      let(:achievement_params) { { title: "title" } }
+      let(:create_achievement) { instance_double(CreateAchievement) }
+      let(:achievement) { instance_double(Achievement) }
+
+      before do
+        allow(CreateAchievement).to receive(:new) { create_achievement }
+        allow(create_achievement).to receive(:create)
+        allow(create_achievement).to receive(:created?)
+        allow(create_achievement).to receive(:achievement) { achievement }
+      end
+
+      it 'sends create message to CreateAchievement' do
+        expect(CreateAchievement).to receive(:new).with(achievement_params, user)
+        expect(create_achievement).to receive(:create)
+        post :create, params: { achievement: achievement_params }
+      end
+
+      context 'achievement is created' do
+        before { allow(create_achievement).to receive(:created?) { true } }
+
+        it 'redirects' do
+          post :create, params: { achievement: achievement_params }
+          expect(response.status).to eq(302)
+        end
+      end
+
+      context 'achievement is not created' do
+        before { allow(create_achievement).to receive(:created?) { false } }
+
+        it 'render :new template' do
+          post :create, params: { achievement: achievement_params }
+          expect(response).to render_template(:new)
+        end
+
+        it 'assigns achievement to template' do
+          post :create, params: { achievement: achievement_params }
+          expect(assigns(:achievement)).to eq(achievement)
+        end
+      end
+    end
+  end
 
 end
