556c23c32097f1c2757e7d36e460ed7f1deb0194
ryanstgermain <ryanjoel26@gmail.com>
2019-06-03 12:37:05 -0600

    refactored code and showed how to use mocking and stubbing. all tests are currently passing

--- mocks_and_stubs/game.rb
+++ mocks_and_stubs/game.rb
@@ -1,31 +1,42 @@
 class Character
-    def initialize(strength: 1, die: Die.new)
-        @strength = strength
-        @die = die
-    end
+  def initialize(strength: 1, die: Die.new, logger: Logger.new)
+    @strength = strength
+    @die = die
+    @logger = logger
+  end
 
-    def climb(difficulty: 10)
-        die.roll + strength >= difficulty
-    end
+  def climb(difficulty: 10)
+    roll = die.roll + strength
+    logger.log("Climbing check. Difficulty: #{difficulty}. Roll: #{roll}")
+    roll >= difficulty
+  end
 
-    private
+  private
 
-    attr_reader :die, :strength
+  attr_reader :die, :strength, :logger
 end
 
 describe Character do
-    describe 'climbing check skill' do
-        let(:die) { double }
-        let(:character) { Character.new(strength: 4, die: die) }
+  describe 'climbing check skill' do
+    let(:die) { double }
+    let(:logger) { double("Logger", log: nil) }
+    let(:character) { Character.new(strength: 5, die: die, logger: logger) }
 
-        it 'climbs successfully when roll + strength is more than difficulty' do
-            allow(die).to receive(:roll) { 11 }
-            expect(character.climb(difficulty: 15)).to be_truthy
-        end
 
-        it 'fails climbing check when roll + strength is less than difficulty' do
-            allow(die).to receive(:roll) { 5 }
-            expect(character.climb(difficulty: 15)).to be_falsy
-        end
+    it 'climbs successfully when roll + strength is more than difficulty' do
+      allow(die).to receive(:roll) { 11 }
+      expect(character.climb(difficulty: 15)).to be_truthy
     end
+
+    it 'fails climbing check when roll + strength is less than difficulty' do
+      allow(die).to receive(:roll) { 5 }
+      expect(character.climb(difficulty: 15)).to be_falsy
+    end
+
+    it 'commands logger to log climb skill check' do
+      allow(die).to receive(:roll) { 7 }
+      expect(logger).to receive(:log).with("Climbing check. Difficulty: 10. Roll: 12")
+      character.climb(difficulty: 10)
+    end
+  end
 end
