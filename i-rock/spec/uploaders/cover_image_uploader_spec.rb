994e4b102aeba90fdf31f41e13e60e5ea42b9799
ryanstgermain <ryanjoel26@gmail.com>
2019-06-04 11:35:27 -0600

    successfully tested to upload images only. all tests are passing

---
+++ i-rock/spec/uploaders/cover_image_uploader_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+require 'spec_helper'
+
+RSpec.describe CoverImageUploader do
+    it 'allows only images' do
+        uploader = CoverImageUploader.new(Achievement.new, :cover_image)
+
+        expect {
+            File.open("#{Rails.root}/spec/fixtures/empty_pdf.pdf") do |f|
+                uploader.store!(f)
+            end
+        }.to raise_exception(CarrierWave::IntegrityError)
+    end
+end
