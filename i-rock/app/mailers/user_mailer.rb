e3af6f04ea33c14996aa4e3a7fb6440aa4c456dd
ryanstgermain <ryanjoel26@gmail.com>
2019-06-04 10:51:16 -0600

    added passing tests for the subject of the email

--- i-rock/app/mailers/user_mailer.rb
+++ i-rock/app/mailers/user_mailer.rb
@@ -1,5 +1,6 @@
 class UserMailer < ApplicationMailer
     def achievement_created(email)
-        mail to: email
+        mail to: email,
+            subject: 'Congratulations with your new achievement!'
     end
 end
--- i-rock/spec/mailers/user_mailer_spec.rb
+++ i-rock/spec/mailers/user_mailer_spec.rb
@@ -1,8 +1,13 @@
 require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
+    let(:email) { UserMailer.achievement_created('author@email.com').deliver_now }
+
     it 'sends achievement created email to author' do
-        email = UserMailer.achievement_created('author@email.com').deliver_now
         expect(email.to).to include('author@email.com')
+    end
+
+    it 'has correct subject' do
+        expect(email.subject).to eq('Congratulations with your new achievement!')
     end
 end
